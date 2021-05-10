53ef8507507299b8754fddd8192ed120b3cdc26c
yangle <yangle@yy.com>
2021-05-10 11:58:41 +0800

    card note button behavior

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 'use strict'
 var data = {
   players: [
-    { id: 1, name: 'player1', school: 'UW', points: 3, assists: 4, rebounds: 5, note: 'he is pretty cool dude' },
-    { id: 2, name: 'player2', school: 'UCLA', points: 3, assists: 4, rebounds: 5, note: 'he is pretty cool dude' },
-    { id: 3, name: 'player3', school: 'WASU', points: 6, assists: 4, rebounds: 5, note: 'he is pretty cool dude' },
-    { id: 4, name: 'player4', school: 'NYU', points: 3, assists: 4, rebounds: 5, note: 'he is pretty cool dude' }
+    { id: 1, name: 'player1', school: 'UW', points: 3, assists: 4, rebounds: 5, note: 'he is pretty cool dude1111111' },
+    { id: 2, name: 'player2', school: 'UCLA', points: 3, assists: 4, rebounds: 5, note: 'he is pretty cool dude22222' },
+    { id: 3, name: 'player3', school: 'WASU', points: 6, assists: 4, rebounds: 5, note: 'he is pretty cool dude333333' },
+    { id: 4, name: 'player4', school: 'NYU', points: 3, assists: 4, rebounds: 5, note: 'he is pretty cool dude4444444' }
   ]
 };
 
@@ -15,17 +15,26 @@ function createDiv(className, content) {
   pTag.textContent = content;
   divTag.classList.add(className);
   divTag.appendChild(pTag);
-  console.log(divTag);
+  // console.log(divTag);
   return divTag;
 }
 
 // create note btn
-function detailBtn() {
+function detailBtn(id) {
   let btn = document.createElement('a');
   btn.type = 'button';
   btn.className = 'btn btn-dark detailBtn';
   btn.textContent = 'Note';
+  btn.id = id;
+  btn.addEventListener('click',showNote);
   return btn;
+}
+
+function showNote() {
+  $('#note').empty();
+  let id = this.id;
+  let note = createDiv("noteDiv", data.players[id - 1].note)
+  $('#note').append(note);
 }
 
 // create li elements for ul
@@ -52,8 +61,8 @@ function createCard(player) {
   let colDiv = document.createElement('div');
   colDiv.classList.add('col-sm');
   colDiv.appendChild(createDiv("player-name", player.name));
-  colDiv.appendChild(createDiv("player-bio", player.school));
-  colDiv.appendChild(detailBtn());
+  colDiv.appendChild(createDiv("player-bio", 'From: ' + player.school));
+  colDiv.appendChild(detailBtn(player.id));
 
   // create second div
   let infoDiv = createDiv('player-info', 'Player Statistics:')
